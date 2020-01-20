9c7e0e2d9016b091a381145c589d11e2dd405636
Diana Ponce <dianap2341@gmail.com>
2020-01-20 00:39:36 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,8 +21,7 @@ def bonus
   }
 
   #code your solution here:
-  def epic_tragedy
-    [:montague][:hero][:status] ="dead"
+epic_tragedy[:montague][:hero][:status] ="dead"
   [:capulet][:heroine][:status] ="dead"
   
 
