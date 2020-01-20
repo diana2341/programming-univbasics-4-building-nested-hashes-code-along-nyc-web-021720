f6a6020ac3d639d130ae95b79a9931d3d63a08f7
Diana Ponce <dianap2341@gmail.com>
2020-01-20 00:33:25 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,9 @@ def bonus
   }
 
   #code your solution here:
-
+def epic_tragedy
+  if
+  
   
 
   #Don't touch the following line! The `bonus` method must return our newly modified epic tragedy hash
